c7456b7f5039beb562c10080f3bf0fd0a16868f5
André Domingos <andre@clicksul.com.br>
2020-07-24 15:32:45 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const espress = require('express');
 const app = express()
 
 app.get('/teste', (req, res) => {
-  return res.json( { hello: 'world' })
+  return res.json( { hello: 'world 2' })
 });
 
 app.listen(3333);
+
+/*
+git add .  para iniciar o git de um ponto
+git comit -m "chore(para expansão do código) fix(para correção) feature(novos códigos) "
+
+*/
